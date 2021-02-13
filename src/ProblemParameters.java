3b943e77b7792effe7806b42b6fc8ea83407fcf6
Erhun Kundakcioglu <erhunk@gmail.com>
2021-02-12 21:46:18 -0500

    v1

--- src/ProblemParameters.java
+++ src/ProblemParameters.java
@@ -5,7 +5,7 @@ public class ProblemParameters {
 	public int numberofCycles = 10;
 	public int numberOfReplications=1000;
 	public double epsilon = 0.0001;
-	public double perturbationRatio = .10;
+	public double perturbationRatio = .15;
 	public SensitivityType st = SensitivityType.RANDOM;
 
 	public String outFile = "output.csv";
